019128f5836a9d69e10bc9c48fa649dc625e5f1a
Aqleem <aqleemorakzai@gmail.com>
2025-02-09 11:02:45 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [input , setInput] = useState("")
+const [name , setName] = useState("")
+const [flag , setFlag] = useState("")
+
+
+
+function getadd(){
+  axios.get(`https://restcountries.com/v3.1/name/${input}`)
+  .then(function (response) {
+    // handle success
+    console.log(response.data[0].flags.png);
+    setName(response.data[0].name.official)
+    setFlag(response.data[0].flags.png)
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
+}
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>GITHub</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='div'>
+   <input type="text" onChange={(e) => setInput(e.target.value)} /> 
+   <br />
+   <button onClick={getadd}>search</button>
+   <h2>{name}</h2>
+   <img src={flag} alt="" />
+
+   </div>
     </>
   )
 }
